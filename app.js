60e23b6c182291063ce1e77706fa43129883c22e
shidqi <nauvalholic@gmail.com>
2019-12-05 22:47:08 +0700

    Add procfile and update ports

--- app.js
+++ app.js
@@ -162,4 +162,4 @@ app.post("/delete", function(req,res){
     
 });
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server started on port successfully"));
